a467fb87073f7f4890e86cdc4ecca73360d73997
nikiandr <nikita.forduy@protonmail.com>
2023-05-20 21:37:50 +0300

    final version

--- app.py
+++ app.py
@@ -4,29 +4,27 @@ import faiss
 import numpy as np
 import json
 
-cosine_similarity_model = SentenceTransformer('sentence-transformers/msmarco-distilbert-cos-v5', device='cpu')
-cosine_similarity_embeddings = np.load('msmarco-distilbert-cos-v5_emb.npy')
-cosine_similarity_index = faiss.IndexFlatIP(768)
-cosine_similarity_index.add(cosine_similarity_embeddings)
-
-dotprod_model = SentenceTransformer('sentence-transformers/msmarco-distilbert-base-tas-b', device='cpu')
-dotprod_embeddings = np.load('msmarco-distilbert-base-tas-b_emb.npy')
-dotprod_index = faiss.IndexFlatIP(768)
-dotprod_index.add(dotprod_embeddings)
+cache = {}
 
 with open('processed_books_metadata.json') as f:
     metadata = json.load(f)
 
-def semantic_search(query, model):
-    if model == "msmarco-distilbert-cos-v5 (cosine similarity)":
-        query_embedding = cosine_similarity_model.encode([query])
-        D, I = cosine_similarity_index.search(query_embedding, 5)
-    elif model == "msmarco-distilbert-base-tas-b (dot product)":
-        query_embedding = dotprod_model.encode([query])
-        D, I = dotprod_index.search(query_embedding, 5)
+def semantic_search(query, model_name):
+    if model_name not in cache:
+        model = SentenceTransformer(f'sentence-transformers/{model_name}', device='cpu')
+        embeddings = np.load(f'{model_name}_emb.npy')
+        index = faiss.IndexFlatIP(embeddings.shape[1])
+        index.add(embeddings)
+        cache[model_name] = (model, index)
+    else:
+        model, index = cache[model_name]
+    query_embedding = model.encode([query])
+    D, I = index.search(query_embedding, 5)
+    # print(D, I)
     D, I = list(D[0]), list(I[0])
     result_list = [(metadata[index]["Name"] + " by " + metadata[index]["Author"], float(distance)) 
          for index, distance in zip(I, D)]
+    # print(result_list)
     result = "".join([f"""### {idx+1}. {result[0]}: {result[1]:.2f}\n""" for idx, result in enumerate(result_list[:5])])
     result = f"""# Top 5 Results\n{result}"""
     return result
@@ -34,9 +32,11 @@ def semantic_search(query, model):
 
 demo = gr.Interface(
     fn=semantic_search, 
-    inputs=[gr.Textbox(label="Query"),
-            gr.Radio(["msmarco-distilbert-cos-v5 (cosine similarity)", "msmarco-distilbert-base-tas-b (dot product)"], 
-                     label="Model")], 
+    inputs=[gr.Textbox(label="Query", value="Phylosophical novel about 2d and 3d worlds"),
+            gr.Dropdown(["msmarco-distilbert-cos-v5", "msmarco-MiniLM-L6-cos-v5", 
+                         "msmarco-MiniLM-L12-cos-v5", "msmarco-distilbert-base-tas-b", 
+                         "msmarco-distilbert-dot-v5", "msmarco-bert-base-dot-v5"], 
+                     label="Model", multiselect=False, value="msmarco-distilbert-base-tas-b")], 
     outputs=gr.Markdown(label="Results"))
     # outputs=gr.Label(num_top_classes=5, label="Cosine Similarity"))
 
