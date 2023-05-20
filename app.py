be16145c8075b210465b1f10ac680910420efa04
nikiandr <nikita.forduy@protonmail.com>
2023-05-20 11:51:50 +0300

    corrected cossim model

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import faiss
 import numpy as np
 import json
 
-cosine_similarity_model = SentenceTransformer('sentence-transformers/msmarco-distilbert-base-v4', device='cpu')
-cosine_similarity_embeddings = np.load('msmarco-distilbert-base-v4_emb.npy')
-cosine_similarity_embeddings_normalized = cosine_similarity_embeddings / np.linalg.norm(cosine_similarity_embeddings, axis=1)[:, None]
+cosine_similarity_model = SentenceTransformer('sentence-transformers/msmarco-distilbert-cos-v5', device='cpu')
+cosine_similarity_embeddings = np.load('msmarco-distilbert-cos-v5_emb.npy')
 cosine_similarity_index = faiss.IndexFlatIP(768)
-cosine_similarity_index.add(cosine_similarity_embeddings_normalized)
+cosine_similarity_index.add(cosine_similarity_embeddings)
 
 dotprod_model = SentenceTransformer('sentence-transformers/msmarco-distilbert-base-tas-b', device='cpu')
 dotprod_embeddings = np.load('msmarco-distilbert-base-tas-b_emb.npy')
@@ -19,14 +18,12 @@ with open('processed_books_metadata.json') as f:
     metadata = json.load(f)
 
 def semantic_search(query, model):
-    match model:
-        case "msmarco-distilbert-base-v4 (cosine similarity)":
-            query_embedding = cosine_similarity_model.encode([query])
-            query_embedding = query_embedding / np.linalg.norm(query_embedding, axis=1)[:, None]
-            D, I = cosine_similarity_index.search(query_embedding, 5)
-        case "msmarco-distilbert-base-tas-b (dot product)":
-            query_embedding = dotprod_model.encode([query])
-            D, I = dotprod_index.search(query_embedding, 5)
+    if model == "msmarco-distilbert-cos-v5 (cosine similarity)":
+        query_embedding = cosine_similarity_model.encode([query])
+        D, I = cosine_similarity_index.search(query_embedding, 5)
+    elif model == "msmarco-distilbert-base-tas-b (dot product)":
+        query_embedding = dotprod_model.encode([query])
+        D, I = dotprod_index.search(query_embedding, 5)
     D, I = list(D[0]), list(I[0])
     result_list = [(metadata[index]["Name"] + " by " + metadata[index]["Author"], float(distance)) 
          for index, distance in zip(I, D)]
@@ -38,7 +35,7 @@ def semantic_search(query, model):
 demo = gr.Interface(
     fn=semantic_search, 
     inputs=[gr.Textbox(label="Query"),
-            gr.Radio(["msmarco-distilbert-base-v4 (cosine similarity)", "msmarco-distilbert-base-tas-b (dot product)"], 
+            gr.Radio(["msmarco-distilbert-cos-v5 (cosine similarity)", "msmarco-distilbert-base-tas-b (dot product)"], 
                      label="Model")], 
     outputs=gr.Markdown(label="Results"))
     # outputs=gr.Label(num_top_classes=5, label="Cosine Similarity"))
